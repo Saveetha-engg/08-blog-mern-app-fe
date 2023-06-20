23a5c5f23ccf6ad042b3f50ec97b659a8710bbf4
sethu-ravichandran <rsethu1997@gmail.com>
2023-06-20 21:13:33 +0530

    Final commit.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
-
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import AddNewBlogComponent from "./components/AddNewBlogComponent/AddNewBlogComponent";
+import EditBlogComponent from "./components/EditBlogComponent/EditBlogComponent";
+import GetAllBlogsComponent from "./components/GetAllBlogsComponent/GetAllBlogsComponent";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+            <div className="container">
+              <h1>Blog App</h1>
+              
+            <nav className="nav-menu">
+                <Link to="/" >Home</Link>
+                <Link to="/admin/add" >Add Blog</Link>
+                <Link to="/admin/edit" >Edit Blog</Link>
+            </nav>
+           <Routes>
+                 <Route exact path='/' element={<GetAllBlogsComponent/>}></Route>
+                 <Route path='/admin/add' element={<AddNewBlogComponent/>}></Route>
+                 <Route path='/admin/edit' element={<EditBlogComponent/>}></Route>
+          </Routes>
+          </div>
+       </Router>
   );
 }
 
